421cd8085383f4bf21ba966177766084de5d538b
Fuhai Gao <Fuh@Fuhais-MacBook-Air.local>
2019-06-15 22:24:58 -0400

    16.3Sum_Closest.cpp

---
+++ 16.3Sum_Closest.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int res = nums[0] + nums[1] + nums[2];
+        sort(nums.begin(), nums.end());
+        for (int i=0; i<nums.size()-2; i++){
+            int start = i+1;
+            int end = nums.size()-1;
+            while (start < end) {
+                int tmp = nums[i] + nums[start] + nums[end];
+                if (tmp < target) start++;
+                else end--;
+                if (abs(tmp-target) < abs(res-target)) res = tmp;
+            }
+        }
+        return res;
+    }
+};
